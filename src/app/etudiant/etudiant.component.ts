7658a8411a90874511c668764e7ce97dfc329e64
ibrahima <brahima1985@gmail.com>
2022-11-08 14:24:18 +0100

    Téléchargement PDF

--- src/app/etudiant/etudiant.component.ts
+++ src/app/etudiant/etudiant.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,ElementRef } from '@angular/core';
 import { EtudiantService } from '../services/etudiant/etudiant.service';
 import { Etudiant } from '../models/Etudiant';
+import {jsPDF} from 'jspdf';
 
 @Component({
   selector: 'app-etudiant',
@@ -12,6 +13,8 @@ export class EtudiantComponent implements OnInit {
   totalRecord:number = 0;
   page : number = 1;
 
+  @ViewChild("content_table") el!: ElementRef;
+
   searchText : any;
 
   etudiants : any = [];
@@ -21,10 +24,33 @@ export class EtudiantComponent implements OnInit {
     this.service.findAll().subscribe((data: Etudiant) => {
       this.etudiants = data;
       this.totalRecord = this.etudiants.lenght;
-    });
+    });   
+   
   }
   
   deleteEtudiant(id : number) {
     
+  }
+
+  MakePDF(){
+    this.service.findAll().subscribe((data: Etudiant) => {
+      this.etudiants = data;
+    }); 
+    
+    let flux  = "<table">
+    ";"
+    let pdf = new jsPDF('p','pt','a4');
+    /*pdf.html(this.el.nativeElement,{
+      callback : (pdf) =>{
+        pdf.save("Liste etudiants.pdf");
+      }
+    })*/
+
+    this.etudiants.forEach((element:Etudiant,i:number) => {
+      pdf.text(
+        element.firstname + " " + element.name+" " +element.email+" " +element.matricule+" " +element.phone+" " +element.formation.libelle+" " +element.formation.niveau+"ième année"
+        ,50,50+(i*50))
+    });
+    pdf.save("Liste etudiants.pdf");
   }
 }
